a020cab39b95916578e67eebe7a14a9ba02807e9
filipusarif <filipus123.gmil@gmail.com>
2024-11-09 17:20:39 +0700

    feat: add neva 22b for image and text understanding

---
+++ neva-22b.py
@@ -0,0 +1,44 @@
+
+import requests, base64
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY_FUYU')
+
+invoke_url = "https://ai.api.nvidia.com/v1/vlm/nvidia/neva-22b"
+stream = True
+
+with open("assets/images/image.jpg", "rb") as f:
+    image_b64 = base64.b64encode(f.read()).decode()
+
+assert len(image_b64) < 180_000, \
+  "To upload larger images, use the assets API (see docs)"
+
+headers = {
+  "Authorization": "Bearer "+ api_key,
+  "Accept": "text/event-stream" if stream else "application/json"
+}
+
+payload = {
+  "messages": [
+    {
+      "role": "user",
+      "content": f'Describe what you see in this image. <img src="data:image/png;base64,{image_b64}" />'
+    }
+  ],
+  "max_tokens": 1024,
+  "temperature": 0.20,
+  "top_p": 0.70,
+  "seed": 0,
+  "stream": stream
+}
+
+response = requests.post(invoke_url, headers=headers, json=payload)
+
+if stream:
+    for line in response.iter_lines():
+        if line:
+            print(line.decode("utf-8"))
+else:
+    print(response.json())
