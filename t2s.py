1269646dd8a0445cba10bdafa085f81accd51da4
filipusarif <filipus123.gmil@gmail.com>
2024-11-10 09:11:14 +0700

    feat: test elevenlabs t2s

---
+++ t2s.py
@@ -0,0 +1,30 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY_T2S')
+
+CHUNK_SIZE = 1024
+url = "https://api.elevenlabs.io/v1/text-to-speech/9BWtsMINqrJLrRacOk9x" 
+
+headers = {
+  "Accept": "audio/mpeg",
+  "Content-Type": "application/json",
+  "xi-api-key": api_key
+}
+
+data = {
+  "text": "Halo, nama saya Arif, saya berbicara dalam Bahasa Indonesia.",
+  "model_id": "eleven_monolingual_v1",
+  "voice_settings": {
+    "stability": 0.75,  
+    "similarity_boost": 0.8  
+  }
+}
+
+response = requests.post(url, json=data, headers=headers)
+with open('output.mp3', 'wb') as f:
+    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+        if chunk:
+            f.write(chunk)
